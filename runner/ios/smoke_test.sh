2ccecabf3c14eff78f718a9512fe50e939540b5e
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-05-05 23:53:16 -0700

    Update smoke_test.sh

--- runner/ios/smoke_test.sh
+++ runner/ios/smoke_test.sh
@@ -1,2 +1,3 @@
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 ##python3 -m invoke test --env='preprod' --lang='en' --app='ios' --deviceOS='iPhone 15:17' --marker='smoke' --appiumServer='local'
+ browserstack-sdk pytest -s ./src/testcases/ios/sanity/boh_login_test.py
