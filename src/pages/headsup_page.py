df2e9b4d210b4ed918bf92067c0f0372a4fbf4e0
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-05-14 22:19:42 -0700

    Update headsup_page.py

--- src/pages/headsup_page.py
+++ src/pages/headsup_page.py
@@ -1,30 +1,45 @@
 import time
 import pytest
+from src.common.shared_workflow import SharedWorkflow
 from src.helpers.app_objects import BoH
-from src.screen_locators.ios_screen_enums import *
-from src.screen_locators.android_screen_enums import *
+import src.screen_locators.ios_screen_enums
+import src.screen_locators.android_screen_enums
+from enum import Enum
 from src.helpers.appium_driver import Driver
 from appium.options.ios import XCUITestOptions
 
+
 class HeadsUpPage(Driver):
+    global locators
+    def __init__(self):
+        global src
+        self.width, self.height = BoH.get_screen_dimension(self)
+        self.offset = 30
+        self.locators = BoH.get_src_screen_enums(self)
+        pass
 
     def verifyInitHeadsUp(self):
-        BoH.wait_until_appear(self, HeadsUpScreen.headsUpTitle, 2)
-        if BoH.is_exist(self, HeadsUpScreen.headsUpText, expected=True):
-            BoH.click(self, HeadsUpScreen.continueButton)
-        BoH.wait_until_appear(self, HeadsUpScreen.warningTitle)
-        if BoH.is_exist(self, HeadsUpScreen.warningTitle, expected=True):
-            BoH.click(self, HeadsUpScreen.ackContinueButton)
+        HeadsUpPage.__init__(self)
+        if self.apps == 'ios':
+            BoH.wait_until_appear(self, self.locators.HeadsUpScreen.headsUpTitle, 2)
+            if BoH.is_exist(self, self.locators.HeadsUpScreen.headsUpText, expected=True):
+                BoH.click(self, self.locators.HeadsUpScreen.continueButton)
+        BoH.wait_until_appear(self, self.locators.HeadsUpScreen.warningTitle)
+        if BoH.is_exist(self, self.locators.HeadsUpScreen.warningTitle, expected=True):
+            BoH.click(self, self.locators.HeadsUpScreen.ackContinueButton)
 
     def verifyUserContentGuideLines(self):
         # validate User Content Guidelines Elements in Page
-        BoH.wait_until_appear(self, HeadsUpScreen.headsUpUserContentTitle, 3)
-        BoH.is_exist(self, HeadsUpScreen.headsUpUserContentText, expected=True)
-        BoH.is_exist(self, HeadsUpScreen.headsUpGuidelines1, expected=True)
-        BoH.is_exist(self, HeadsUpScreen.headsUpGuidelines2, expected=True)
-        BoH.is_exist(self, HeadsUpScreen.logoutButton, expected=True)
-        BoH.is_exist(self, HeadsUpScreen.privacyPolicy, expected=True)
-        BoH.is_exist(self, HeadsUpScreen.termsOfUse, expected=True)
-        if BoH.is_exist(self, HeadsUpScreen.acceptButton, expected=True):
-            BoH.click(self, HeadsUpScreen.acceptButton)
+        HeadsUpPage.__init__(self)
+        BoH.wait_until_appear(self, self.locators.HeadsUpScreen.headsUpUserContentTitle, 5)
+        BoH.is_exist(self, self.locators.HeadsUpScreen.headsUpUserContentText, expected=True)
+        BoH.is_exist(self, self.locators.HeadsUpScreen.headsUpGuidelines1, expected=True)
+        BoH.is_exist(self, self.locators.HeadsUpScreen.headsUpGuidelines2, expected=True)
+        BoH.is_exist(self, self.locators.HeadsUpScreen.logoutButton, expected=True)
+        BoH.is_exist(self, self.locators.HeadsUpScreen.privacyPolicy, expected=True)
+        BoH.is_exist(self, self.locators.HeadsUpScreen.termsOfUse, expected=True)
+        if BoH.is_exist(self, self.locators.HeadsUpScreen.acceptButton, expected=True):
+            BoH.click(self, self.locators.HeadsUpScreen.acceptButton)
+
+
 
