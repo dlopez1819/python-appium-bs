fc010d5b648bd8128baac63e9e901789ca692dfe
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-05-14 22:19:10 -0700

    Update alerts_page.py

--- src/pages/alerts_page.py
+++ src/pages/alerts_page.py
@@ -1,20 +1,26 @@
 import pytest
-from src.screen_locators.android_screen_enums import *
-from src.screen_locators.ios_screen_enums import *
+import src.screen_locators.ios_screen_enums
+import src.screen_locators.android_screen_enums
+from enum import Enum
 from src.helpers.app_objects import BoH
 from src.helpers.appium_driver import Driver
 
 class AlertsPage(Driver):
+    global locators
+    def __init__(self):
+        global src
+        self.locators = BoH.get_src_screen_enums(self)
 
     def allowSendNotificationAlert(self):
-        if BoH.is_exist(self, AlertsScreen.bohSendNotificationAlert, expected=True):
-            BoH.click(self, AlertsScreen.allowButton)
+        if BoH.is_exist(self, self.locators.AlertsScreen.bohSendNotificationAlert, expected=True):
+            BoH.click(self, self.locators.AlertsScreen.allowButton)
 
     def allowLocationAlert(self):
-        if BoH.is_exist(self, AlertsScreen.locationAlert, expected=True):
-            BoH.click(self, AlertsScreen.allowOnceButton)
+        AlertsPage.__init__(self)
+        if BoH.is_exist(self, self.locators.AlertsScreen.locationAlert, expected=True):
+            BoH.click(self,self.locators.AlertsScreen.allowOnceButton)
 
     def authBOHSignInAlert(self):
-        if BoH.is_exist(self, AlertsScreen.bohAuthAlert, expected=True):
-            BoH.click(self, AlertsScreen.authContinueButton)
-        BoH.wait_until_disappear(self, AlertsScreen.bohAuthAlert, 5)
+        if BoH.is_exist(self, self.locators.AlertsScreen.bohAuthAlert, expected=True):
+            BoH.click(self,self.locators. AlertsScreen.authContinueButton)
+        BoH.wait_until_disappear(self, self.locators.AlertsScreen.bohAuthAlert, 5)
