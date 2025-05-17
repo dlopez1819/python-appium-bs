5630c1d2727634ee5b8923308d44b5a741228bb2
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-05-17 12:44:40 -0700

    Update appium_driver.py

--- src/helpers/appium_driver.py
+++ src/helpers/appium_driver.py
@@ -218,8 +218,8 @@ class Driver():
     @pytest.fixture(autouse=True)
     #def cli(self, app, device, appiumserver, os, env, get_logger):  # local
     def cli(self, apps, device, appiumserver, os, env, get_logger):
-        self.app = app  # BS
-        #self.apps = apps  #local
+        #self.app = app  # local
+        self.apps = apps  #BS
         self.device = device
         self.os = os
         self.logger = get_logger
