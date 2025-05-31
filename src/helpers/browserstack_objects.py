23ec7eb8802c5f5764c2ee04e08cf54e72d37ade
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-05-30 19:39:48 -0700

    Update browserstack_objects.py

--- src/helpers/browserstack_objects.py
+++ src/helpers/browserstack_objects.py
@@ -8,7 +8,7 @@ class BrowserstackObjects(Driver):
     def __init__(self):
         self.locators = BoH.get_src_screen_enums(self)
 
-    def enter_BS_Username(self, test_email):
+    def enter_bs_username(self, test_email):
         BrowserstackObjects.__init__(self)
         if self.apps == 'android':
             if BoH.is_exist(self, self.locators.loginOktaScreen.loginTab_BS, True):
@@ -19,22 +19,41 @@ class BrowserstackObjects(Driver):
                 BoH.click(self, self.locators.loginOktaScreen.loginTab)
             BoH.send_keys(self, self.locators.loginOktaScreen.username, test_email)
 
-    def enter_BS_Password(self, test_pwd):
+    def enter_bs_password(self, test_pwd):
         BrowserstackObjects.__init__(self)
         if self.apps == 'android':
             BoH.send_keys(self, self.locators.loginOktaScreen.password_BS, test_pwd)
         else:
             BoH.send_keys(self, self.locators.loginOktaScreen.password, test_pwd)
 
-    def tap_BS_LoginButton(self):
+    def tap_bs_login_button(self):
         BrowserstackObjects.__init__(self)
         if self.apps == 'android':
             BoH.click(self, self.locators.loginOktaScreen.loginButton_BS)
         else:
             BoH.click(self, self.locators.loginOktaScreen.loginButton)
 
-    def swipe_BS_Page(self):
+    def swipe_bs_page(self):
         BrowserstackObjects.__init__(self)
         if self.apps == 'ios':
             BoH.click(self, self.locators.loginOktaScreen.keyboardDone)
             self.driver.execute_script('mobile: scroll', {'direction': 'down'})
+
+     def swipe_bs_page(self):
+        BrowserstackObjects.__init__(self)
+        if self.apps == 'ios':
+            BoH.click(self, self.locators.loginOktaScreen.keyboardDone)
+            self.driver.execute_script('mobile: scroll', {'direction': 'down'})
+
+    def scroll_bs_down(self):
+        BrowserstackObjects.__init__(self)
+        if self.apps == 'ios':
+            self.driver.execute_script('mobile: scroll', {'direction': 'down'})
+        else:
+            self.driver.execute_script("mobile:swipe", {"direction": "down"})
+
+    def scroll_bs_up(self):
+        if self.apps == 'ios':
+            self.driver.execute_script('mobile: scroll', {'direction': 'up'})
+        else:
+            self.driver.execute_script("mobile:swipe", {"direction": "up"})
