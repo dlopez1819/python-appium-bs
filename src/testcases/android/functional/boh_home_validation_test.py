de53a7241469569fabc09a08453d07fb770fd92d
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-05-30 20:28:44 -0700

    Create boh_home_validation_test.py

---
+++ src/testcases/android/functional/boh_home_validation_test.py
@@ -0,0 +1,47 @@
+import pytest
+from src.common.shared_workflow import SharedWorkflow
+from src.helpers.app_objects import BoH
+from src.helpers.appium_driver import Driver
+import allure
+from src.pages.headsup_page import HeadsUpPage
+from src.pages.home_page import HomePage
+from src.pages.login_page import LoginPage
+from src.pages.menu_page import MenuPage
+from src.pages.user_guidelines_page import UserGuideLinesPage
+
+class Account:
+    BoHCredentials = [
+        # BoHCredentials pre-prod user
+        ['diego11.lopez@nttdata.com', 'Password1']
+    ]
+
+class TestBoHHome(Driver):
+    global flagLogin #userLogged
+
+    def setUp(self):
+        super().setUp()
+        self.width, self.height = BoH.get_screen_dimension(self)
+        self.offset = 30
+        SharedWorkflow.verifySplash(self)
+
+    def teardown_method(self):
+        super().tearDown()
+
+    @pytest.mark.functional
+    @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
+    def test_boh_home_featured_BOH19_TC651(self, test_email, test_pwd):
+        # TEST CASES: BOH19-TC-651, BOH19-TC-2522, BOH19-TC-2518, BOH21-TC-2577
+        if LoginPage.isNormalUserLoggedIn(self) is False:
+            LoginPage.oktaUserLoginSkip(self, test_email, test_pwd)
+        else:
+            UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
+        # verify Featured from HomePage
+        UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
+        HeadsUpPage.verifyVehicleProfile(self)
+        HomePage.homePage(self)
+        HomePage.homeLocation(self)
+        SharedWorkflow.scrollDown(self, self.width - 100, (self.height / 2 + self.offset), self.width - 100, self.offset) # scroll half page
+        HomePage.homeViewLeaderboard(self)
+        HomePage.homeFeaturedTab(self)
+        HomePage.homeTrailDetails(self)
+      
