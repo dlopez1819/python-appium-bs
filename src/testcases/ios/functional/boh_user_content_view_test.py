1ebc2d132137146cb7dfc2b0e101314e36c7bf29
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-05-30 20:35:46 -0700

    Create boh_user_content_view_test.py

---
+++ src/testcases/ios/functional/boh_user_content_view_test.py
@@ -0,0 +1,37 @@
+import pytest
+from src.common.shared_workflow import SharedWorkflow
+from src.helpers.appium_driver import Driver
+import allure
+from src.pages.login_page import LoginPage
+from src.pages.user_guidelines_page import UserGuideLinesPage
+
+class Account:
+    BoHCredentials = [
+        # BoHCredentials pre-prod user
+        ['diego11.lopez@nttdata.com', 'Password1']
+    ]
+
+class TestBoHUserContent(Driver):
+    global flagLogin #userLogged
+
+    def setUp(self):
+        super().setUp()
+        SharedWorkflow.verifySplash(self)
+
+    def teardown_method(self):
+        super().tearDown()
+
+    @pytest.mark.functional
+    @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
+    def test_boh_user_content_guidelines_BOH19_TC2540(self, test_email, test_pwd):
+        # TEST CASES: BOH19-TC-2540
+        if LoginPage.isNormalUserLoggedIn(self) is False:
+            LoginPage.oktaUserLoginSkip(self, test_email, test_pwd)
+        else:
+            UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
+        # verify UserGuideLinesPage
+        UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin = False)
+        UserGuideLinesPage.verifyTermsOfUse(self)
+        UserGuideLinesPage.verifyPrivacyPolicyPage(self)
+        #UserGuideLinesPage.logout(self)
+        #LoginPage.assertIfLoginPage(self)
