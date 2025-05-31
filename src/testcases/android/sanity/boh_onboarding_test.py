4aed2a1850972fab2d8679fadb232476aeb71156
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-05-30 20:32:54 -0700

    Create boh_onboarding_test.py

---
+++ src/testcases/android/sanity/boh_onboarding_test.py
@@ -0,0 +1,33 @@
+import pytest
+import allure
+from src.common.shared_workflow import SharedWorkflow
+from src.helpers.appium_driver import Driver
+from src.pages.login_page import LoginPage
+from src.pages.user_guidelines_page import UserGuideLinesPage
+
+
+class Account:
+    BoHCredentials = [
+        # BoHCredentials pre-prod user
+        ['diego11.lopez@nttdata.com', 'Password1']
+    ]
+    BoHEnvironments = ('dev', 'test', 'preprod', 'prod')
+
+class TestBoHOnBoarding(Driver):
+    global flagLogin  # userLogged
+    def setUp(self):
+       super().setUp()
+       SharedWorkflow.verifySplash(self)
+
+    def teardown_method(self):
+        super().tearDown()
+
+    @pytest.mark.sanity
+    @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
+    # TEST CASES: BOH19-TC-575. App On-Boarding - Skip
+    def test_boh_onboarding_skip_BOH19_TC575(self, test_email, test_pwd):
+        if LoginPage.isNormalUserLoggedIn(self) is False:
+            LoginPage.onboardingLoginSkip(self, test_email, test_pwd)
+        else:
+            UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
+        LoginPage.assertIfLoginPage(self)
