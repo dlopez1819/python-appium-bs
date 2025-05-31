9907910d9b2f063b3e659566a104481d91343569
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-05-30 19:31:07 -0700

    Update functional_test.sh

--- runner/ios/functional_test.sh
+++ runner/ios/functional_test.sh
@@ -1,1 +1,7 @@
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+# FOR RUNNING TEST ON LOCAL SERVER
+#python3 -m invoke test --env='preprod' --lang='en' --app='ios' --deviceOS='iPhone 15:17' --marker='sanity' --appiumServer='local'
+
+# BROWSERSTACK CLOUD SERVER
+python3 -m invoke test --env='preprod' --lang='en' --app='ios' --deviceOS='iPhone 15:17' --marker='functional' --appiumServer='browserstack'
 
