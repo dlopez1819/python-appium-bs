074950ede7770fd71fd2ca267d15691115ee2637
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-04 22:46:20 -0700

    Update boh_guest_featured_trail_enhancements_test.py

--- src/testcases/ios/functional/boh_guest_featured_trail_enhancements_test.py
+++ src/testcases/ios/functional/boh_guest_featured_trail_enhancements_test.py
@@ -14,7 +14,7 @@ class Account:
         ['diego11.lopez@nttdata.com', 'Password1']
     ]
 
-class TestBoHHome(Driver):
+class TestBoHGuestFeaturedTrail(Driver):
     global flagLogin #userLogged
 
     def setUp(self):
