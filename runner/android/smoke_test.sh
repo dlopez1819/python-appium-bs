ab8096ee93f993dcf6784c70dcb61e6e3102cad7
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-05 14:40:59 -0700

    Update smoke_test.sh

--- runner/android/smoke_test.sh
+++ runner/android/smoke_test.sh
@@ -3,5 +3,5 @@ export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 #python3 -m invoke test --env='preprod' --lang='en' --app='ios' --deviceOS='iPhone 15:17' --marker='sanity' --appiumServer='local'
 
 # BROWSERSTACK CLOUD SERVER
-python3 -m invoke test --env='preprod' --lang='en' --app='android' --deviceOS='Samsung Galaxy S22:12.0' --marker='sanity' --appiumServer='browserstack'
+python3 -m invoke test --env='preprod' --lang='en' --app='android' --deviceOS='Samsung Galaxy S22:12.0' --marker='smoke' --appiumServer='browserstack'
 #browserstack-sdk pytest -s boh_login_test.py
