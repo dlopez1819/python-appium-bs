0eb72454d876528700cff607d843db3eae276e5e
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-04 19:35:40 -0700

    Update location_page.py

--- src/pages/location_page.py
+++ src/pages/location_page.py
@@ -10,11 +10,4 @@ class LocationPage(Driver):
 
     def verifyLocationServices(self):
         LocationPage.__init__(self)
-        if BoH.is_exist(self, self.locators.LocationScreen.sorryLocationMessage, expected=True):
-            assert (BoH.is_exist(self, self.locators.LocationScreen.sorryLocationMessage)) is True, "User location service is disabled for BoH"
-            BoH.click(self, self.locators.LocationScreen.okLocationButton)
-        else:
-            if self.apps == 'android':
-                BoH.click(self, self.locators.LocationScreen.closeButton)
-            else:
-                BoH.tap_by_coordinates(self, 25, 70)
+      
