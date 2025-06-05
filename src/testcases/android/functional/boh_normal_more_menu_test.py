cbf886ae09dace8b07d9971351b97fe11a1774ee
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-05 10:37:06 -0700

    Update boh_normal_more_menu_test.py

--- src/testcases/android/functional/boh_normal_more_menu_test.py
+++ src/testcases/android/functional/boh_normal_more_menu_test.py
@@ -1,6 +1,5 @@
 import pytest
 from src.common.shared_workflow import SharedWorkflow
-from src.helpers.app_objects import BoH
 from src.helpers.appium_driver import Driver
 import allure
 from src.pages.headsup_page import HeadsUpPage
