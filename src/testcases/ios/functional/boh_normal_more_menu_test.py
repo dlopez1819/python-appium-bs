cf90d69f87bf5b6a73f84e7976a5d4aa8f6b7818
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-04 22:44:29 -0700

    Create boh_normal_more_menu_test.py

---
+++ src/testcases/ios/functional/boh_normal_more_menu_test.py
@@ -0,0 +1,47 @@
+import pytest
+from src.common.shared_workflow import SharedWorkflow
+from src.helpers.app_objects import BoH
+from src.helpers.appium_driver import Driver
+import allure
+from src.pages.headsup_page import HeadsUpPage
+from src.pages.home_page import HomePage
+from src.pages.login_page import LoginPage
+from src.pages.menu_page import MenuPage
+from src.pages.terms_of_use_page import TermsOfUsePage
+from src.pages.user_guidelines_page import UserGuideLinesPage
+
+
+class Account:
+    BoHCredentials = [
+        # BoHCredentials pre-prod user
+        ['diego11.lopez@nttdata.com', 'Password1']
+    ]
+
+class TestBoHNormalMoreMenu(Driver):
+    global flagLogin #userLogged
+
+    def setUp(self):
+        super().setUp()
+        SharedWorkflow.verifySplash(self)
+
+    def teardown_method(self):
+        super().tearDown()
+
+    @pytest.mark.regression
+    @pytest.mark.functional
+    @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
+    def test_boh_normal_more_menu_terms_of_use_BOH19_TC536(self, test_email, test_pwd):
+        # TEST CASES: BOH19-TC-536
+        self.locators = BoH.get_src_screen_enums(self)
+        if LoginPage.isNormalUserLoggedIn(self) is False:
+            LoginPage.oktaUserLoginSkip(self, test_email, test_pwd)
+        else:
+            UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
+        UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
+        HeadsUpPage.verifyVehicleProfile(self)
+        HeadsUpPage.checkTrailAndBadges(self, flagLogin=True)
+        HomePage.assertIfHomenPage(self)
+        MenuPage.termsOfUse(self)
+        TermsOfUsePage.verifyTermsOfUsePage(self)
+        #MenuPage.logout(self)
+        #LoginPage.assertIfLoginPage(self)
