cba07e5081cfcd7abc23856e57d0f528b8f3be8d
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-11 23:08:49 -0700

    Update boh_onboarding_test.py

--- src/testcases/ios/sanity/boh_onboarding_test.py
+++ src/testcases/ios/sanity/boh_onboarding_test.py
@@ -1,6 +1,7 @@
 import pytest
 from src.common.shared_workflow import SharedWorkflow
 from src.helpers.appium_driver import Driver
+from src.pages.headsup_page import HeadsUpPage
 from src.pages.login_page import LoginPage
 from src.pages.user_guidelines_page import UserGuideLinesPage
 
@@ -23,7 +24,7 @@ class TestBoHOnBoarding(Driver):
     @pytest.mark.regression
     @pytest.mark.sanity
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    # TEST CASES: BOH19-TC-2568 : App On-Boarding (Tutorial)
+    # TEST CASES: BOH19-TC-2568. App On-Boarding (Tutorial)
     def test_boh_onboarding_tutorial_BOH19_TC2568(self, test_email, test_pwd):
         if LoginPage.isNormalUserLoggedIn(self) is False:
             LoginPage.onboardingLoginTutorial(self, test_email, test_pwd)
@@ -34,10 +35,34 @@ class TestBoHOnBoarding(Driver):
     @pytest.mark.regression
     @pytest.mark.sanity
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    # TEST CASES:  BOH19-TC-575. BOH19-TC-574 : App On-Boarding - Skip
+    # TEST CASES: BOH19-TC-575. App On-Boarding skip (Tutorial)
     def test_boh_onboarding_skip_BOH19_TC575(self, test_email, test_pwd):
         if LoginPage.isNormalUserLoggedIn(self) is False:
             LoginPage.onboardingLoginSkip(self, test_email, test_pwd)
         else:
             UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
         LoginPage.assertIfLoginPage(self)
+
+    @pytest.mark.regression
+    @pytest.mark.sanity
+    @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
+    # TEST CASES: BOH19-TC-574. app On-Boarding
+    def test_boh_app_onboarding_BOH19_TC574(self, test_email, test_pwd):
+        if LoginPage.isNormalUserLoggedIn(self) is False:
+            LoginPage.onboardingLoginTutorial(self, test_email, test_pwd)
+        else:
+            UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
+        LoginPage.assertIfLoginPage(self)
+
+    @pytest.mark.regression
+    @pytest.mark.sanity
+    @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
+    # TEST CASES: BOH19-TC-2569
+    def test_boh_onboarding_email_login_BOH19_TC2569(self, test_email, test_pwd):
+        if LoginPage.isNormalUserLoggedIn(self) is False:
+            LoginPage.oktaUserLogin(self, test_email, test_pwd)
+        else:
+            UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
+        UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
+        HeadsUpPage.verifyVehicleProfile(self)
+    
