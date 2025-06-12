d4baab79ff7e6187e1289399c76709d005ce75b2
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-11 23:45:29 -0700

    Update boh_login_test.py

--- src/testcases/ios/sanity/boh_login_test.py
+++ src/testcases/ios/sanity/boh_login_test.py
@@ -22,7 +22,7 @@ class TestBoHLogin(Driver):
     def teardown_method(self):
         super().tearDown()
 
-    #@pytest.mark.regression
+    @pytest.mark.regression
     @pytest.mark.sanity
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
     # TEST CASES: BOH19-TC-666, Okta Login Test
