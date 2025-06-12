f2c094065d09a370d16a050fa4b797ec6034a48e
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-11 23:30:04 -0700

    Update boh_login_test.py

--- src/testcases/android/sanity/boh_login_test.py
+++ src/testcases/android/sanity/boh_login_test.py
@@ -51,3 +51,17 @@ class TestBoHLogin(Driver):
         MenuPage.guestLogOut(self)
         LoginPage.assertIfLoginPage(self)
 
+    @pytest.mark.regression
+    @pytest.mark.sanity
+    @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
+    # TEST CASES:  BOH19-TC-2572
+    def test_boh_logout_email_BOH19_TC2572(self, test_email, test_pwd):
+        if LoginPage.isNormalUserLoggedIn(self) is False:
+            LoginPage.oktaUserLogin(self, test_email, test_pwd)
+        else:
+            UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
+        UserGuideLinesPage.verifyUserContentGuideLines(self, flagLogin=True)
+        HeadsUpPage.verifyVehicleProfile(self)
+        MenuPage.logout(self)
+        LoginPage.assertIfLoginPage(self)
+
