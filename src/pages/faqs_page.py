55d977494245f110c3b673c3afd631ab6487922a
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-21 23:08:41 -0700

    Update faqs_page.py

--- src/pages/faqs_page.py
+++ src/pages/faqs_page.py
@@ -1,5 +1,6 @@
 from src.helpers.app_objects import BoH
 from src.helpers.appium_driver import Driver
+from src.common.shared_workflow import SharedWorkflow
 
 class FaqsPage(Driver):
 
@@ -13,10 +14,6 @@ class FaqsPage(Driver):
         BoH.wait_until_appear(self, self.locators.FAQsScreen.faqsTitle, 5)
         BoH.is_exist(self, self.locators.FAQsScreen.faqsTitle, expected=True)
         BoH.is_exist(self, self.locators.FAQsScreen.contentBodyText, expected=True)
-        if self.apps == 'ios':
-            BoH.tap_by_coordinates(self, 25, 70)
-        else:
-            BoH.tap_by_coordinates(self, 85, 105)
 
     def closeFaqsPage(self):
-        BoH.click(self, self.locators.DisclaimerScreen.closeButton)
+        SharedWorkflow.closePage(self)
