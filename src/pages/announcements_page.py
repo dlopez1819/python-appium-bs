09d6299af8f772ddd8aaded27f50e896e3d8b995
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-21 22:53:57 -0700

    Create announcements_page.py

---
+++ src/pages/announcements_page.py
@@ -0,0 +1,19 @@
+from src.helpers.app_objects import BoH
+from src.helpers.appium_driver import Driver
+
+class AnnouncementsPage(Driver):
+
+    def __init__(self):
+        self.width, self.height = BoH.get_screen_dimension(self)
+        self.offset = 30
+        self.locators = BoH.get_src_screen_enums(self)
+
+    def verifyAnnouncements(self):
+        AnnouncementsPage.__init__(self)
+        BoH.wait_until_appear(self, self.locators.AnnouncementsScreen.announcementsTitle, 5)
+        BoH.is_exist(self, self.locators.AnnouncementsScreen.announcementsTitle, expected=True)
+        # Validate if list of announcements is not empty
+        assert len(BoH.get_list_elements(self, self.locators.AnnouncementsScreen.announcementsContainerList)) != 0
+
+    def closeAnnouncementsPage(self):
+        SharedWorkflow.closePage(self)
