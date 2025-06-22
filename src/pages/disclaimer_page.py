dfe3fb6e9612822ae5d334efbba862043cd70c20
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-21 23:15:37 -0700

    Update disclaimer_page.py

--- src/pages/disclaimer_page.py
+++ src/pages/disclaimer_page.py
@@ -1,5 +1,6 @@
 from src.helpers.app_objects import BoH
 from src.helpers.appium_driver import Driver
+from src.common.shared_workflow import SharedWorkflow
 
 class DisclaimerPage(Driver):
 
@@ -13,7 +14,6 @@ class DisclaimerPage(Driver):
         BoH.wait_until_appear(self, self.locators.DisclaimerScreen.disclaimerTitle, 5)
         BoH.is_exist(self, self.locators.DisclaimerScreen.disclaimerTitle, expected=True)
         BoH.is_exist(self, self.locators.DisclaimerScreen.contentBodyText, expected=True)
-        DisclaimerPage.closeDisclaimerPage(self)
 
     def closeDisclaimerPage(self):
-        BoH.click(self, self.locators.DisclaimerScreen.closeButton)
+        SharedWorkflow.closePage(self)
