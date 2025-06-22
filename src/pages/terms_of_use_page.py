a10d0894cce6813652d6ab80bfff1d6af7754226
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-21 23:14:23 -0700

    Update terms_of_use_page.py

--- src/pages/terms_of_use_page.py
+++ src/pages/terms_of_use_page.py
@@ -1,5 +1,6 @@
 from src.helpers.app_objects import BoH
 from src.helpers.appium_driver import Driver
+from src.common.shared_workflow import SharedWorkflow
 
 class TermsOfUsePage(Driver):
 
@@ -13,7 +14,6 @@ class TermsOfUsePage(Driver):
         BoH.wait_until_appear(self, self.locators.TermsOfUseScreen.termOfUseTitle, 5)
         BoH.is_exist(self, self.locators.TermsOfUseScreen.termOfUseTitle, expected=True)
         BoH.is_exist(self, self.locators.TermsOfUseScreen.contentBodyText, expected=True)
-        TermsOfUsePage.closeTermsOfUsePage(self)
 
     def closeTermsOfUsePage(self):
-        BoH.click(self, self.locators.TermsOfUseScreen.closeButton)
+        SharedWorkflow.closePage(self)
