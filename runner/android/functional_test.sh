45a8ed6541d4c21ec0e413b7d2bdf697337cc029
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-06-30 19:30:46 -0700

    Update functional_test.sh

--- runner/android/functional_test.sh
+++ runner/android/functional_test.sh
@@ -3,5 +3,5 @@ export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 #python3 -m invoke test --env='preprod' --lang='en' --app='ios' --deviceOS='iPhone 15:17' --marker='sanity' --appiumServer='local'
 
 # BROWSERSTACK CLOUD SERVER
-python3 -m invoke test --env='preprod' --lang='en' --app='android' --deviceOS='Samsung Galaxy S22:12.0' --marker='functional22' --appiumServer='browserstack'
+python3 -m invoke test --env='preprod' --lang='en' --app='android' --deviceOS='Samsung Galaxy S22:12.0' --marker='functional2' --appiumServer='browserstack'
 
