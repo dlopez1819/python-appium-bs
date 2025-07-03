80b0cc3652acc52399a4b2e25df8432fbd647d8b
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-07-02 22:40:35 -0700

    Update boh_trail_and_map_test.py

--- src/testcases/android/functional/boh_trail_and_map_test.py
+++ src/testcases/android/functional/boh_trail_and_map_test.py
@@ -25,7 +25,7 @@ class TestBoHTrailAndMap(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_tray_list_results_BOH19_TC643(self, test_email, test_pwd):
+    def test_tray_list_results_BOH19_TC643(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC643
         BaseClassAppManager.userLoginSkipHeadsUp(self,test_email, test_pwd)
         HomePage.assertIfHomePage(self)
@@ -35,7 +35,7 @@ class TestBoHTrailAndMap(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_collapsed_tray_results_BOH19_TC2586(self, test_email, test_pwd):
+    def test_collapsed_tray_results_BOH19_TC2586(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC2586
         BaseClassAppManager.userLoginSkipHeadsUp(self,test_email, test_pwd)
         HomePage.assertIfHomePage(self)
@@ -47,7 +47,7 @@ class TestBoHTrailAndMap(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_card_display_BOH19_TC2587(self, test_email, test_pwd):
+    def test_card_display_BOH19_TC2587(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC2587
         BaseClassAppManager.userLoginSkipHeadsUp(self,test_email, test_pwd)
         HomePage.assertIfHomePage(self)
@@ -58,7 +58,7 @@ class TestBoHTrailAndMap(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_tray_card_event_challenge_BOH19_TC2590(self, test_email, test_pwd):
+    def test_tray_card_event_challenge_BOH19_TC2590(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC2590
         BaseClassAppManager.userLoginSkipHeadsUp(self, test_email, test_pwd)
         HomePage.assertIfHomePage(self)
@@ -68,7 +68,7 @@ class TestBoHTrailAndMap(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_trail_details_BOH19_TC639(self, test_email, test_pwd):
+    def test_trail_details_BOH19_TC639(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC639
         BaseClassAppManager.userLoginSkipHeadsUp(self, test_email, test_pwd)
         HomePage.assertIfHomePage(self)
@@ -79,7 +79,7 @@ class TestBoHTrailAndMap(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_multiple_pin_map_result_map_BOH19_TC2600(self, test_email, test_pwd):
+    def test_multiple_pin_map_result_map_BOH19_TC2600(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC2600
         BaseClassAppManager.userLoginSkipHeadsUp(self, test_email, test_pwd)
         HomePage.assertIfHomePage(self)
@@ -89,7 +89,7 @@ class TestBoHTrailAndMap(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_swipe_card_event_challenge_map_BOH19_TC2591(self, test_email, test_pwd):
+    def test_swipe_card_event_challenge_map_BOH19_TC2591(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC2591
         BaseClassAppManager.userLoginSkipHeadsUp(self, test_email, test_pwd)
         HomePage.assertIfHomePage(self)
@@ -99,7 +99,7 @@ class TestBoHTrailAndMap(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_pin_map_card_details_BOH19_TC2601(self, test_email, test_pwd):
+    def test_pin_map_card_details_BOH19_TC2601(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC2601
         BaseClassAppManager.userLoginSkipHeadsUp(self, test_email, test_pwd)
         HomePage.assertIfHomePage(self)
@@ -111,7 +111,7 @@ class TestBoHTrailAndMap(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_search_trail_BOH19_TC641_and_BOH19_TC2585(self, test_email, test_pwd):
+    def test_search_trail_BOH19_TC641_and_BOH19_TC2585(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC641 & BOH19-TC2585
         search_trail = 'Twisted'
         BaseClassAppManager.userLoginSkipHeadsUp(self, test_email, test_pwd)
@@ -124,7 +124,7 @@ class TestBoHTrailAndMap(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_all_trail_events_BOH19_TC651(self, test_email, test_pwd):
+    def test_all_trail_events_BOH19_TC651(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC651
         search_trail = 'Twisted'
         BaseClassAppManager.userLoginSkipHeadsUp(self, test_email, test_pwd)
