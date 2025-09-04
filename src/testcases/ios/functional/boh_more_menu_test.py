40f66a04b9fe209fc1ecdd59e6cf10bf1023e789
dlopez1819 <71575702+dlopez1819@users.noreply.github.com>
2025-09-03 19:20:40 -0700

    Update and rename boh_normal_more_menu_test.py to boh_more_menu_test.py

--- src/testcases/ios/functional/boh_more_menu_test.py
+++ src/testcases/ios/functional/boh_more_menu_test.py
@@ -22,7 +22,7 @@ class Account:
         ['diego11.lopez@nttdata.com', 'Password1']
     ]
 
-class TestBoHNormalMoreMenu(Driver):
+class TestBoHMoreMenu(Driver):
     global flagLogin #userLogged
 
     def setUp(self):
@@ -35,7 +35,7 @@ class TestBoHNormalMoreMenu(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_normal_more_menu_terms_of_use_BOH19_TC536(self, test_email, test_pwd):
+    def test_boh_more_menu_terms_of_use_BOH19_TC536(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC-536
         if LoginPage.isNormalUserLoggedIn(self) is False:
             LoginPage.oktaUserLoginSkip(self, test_email, test_pwd)
@@ -51,7 +51,7 @@ class TestBoHNormalMoreMenu(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_normal_more_menu_disclaimers_BOH19_TC539(self, test_email, test_pwd):
+    def test_boh_more_menu_disclaimers_BOH19_TC539(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC-539
         if LoginPage.isNormalUserLoggedIn(self) is False:
             LoginPage.oktaUserLoginSkip(self, test_email, test_pwd)
@@ -66,7 +66,7 @@ class TestBoHNormalMoreMenu(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_normal_more_menu_faqs_BOH19_TC541(self, test_email, test_pwd):
+    def test_boh_more_menu_faqs_BOH19_TC541(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC-541
         if LoginPage.isNormalUserLoggedIn(self) is False:
             LoginPage.oktaUserLoginSkip(self, test_email, test_pwd)
@@ -81,7 +81,7 @@ class TestBoHNormalMoreMenu(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_normal_more_menu_off_roading_101_BOH19_TC2594(self, test_email, test_pwd):
+    def test_boh_more_menu_off_roading_101_BOH19_TC2594(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC-2594
         if LoginPage.isNormalUserLoggedIn(self) is False:
             LoginPage.oktaUserLoginSkip(self, test_email, test_pwd)
@@ -97,7 +97,7 @@ class TestBoHNormalMoreMenu(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_normal_more_menu_announcements_BOH19_TC2595(self, test_email, test_pwd):
+    def test_boh_more_menu_announcements_BOH19_TC2595(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC-2595
         if LoginPage.isNormalUserLoggedIn(self) is False:
             LoginPage.oktaUserLoginSkip(self, test_email, test_pwd)
@@ -113,7 +113,7 @@ class TestBoHNormalMoreMenu(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_normal_more_menu_suggest_a_trail_BOH19_TC2596(self, test_email, test_pwd):
+    def test_boh_more_menu_suggest_a_trail_BOH19_TC2596(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC-2596
         if LoginPage.isNormalUserLoggedIn(self) is False:
             LoginPage.oktaUserLoginSkip(self, test_email, test_pwd)
@@ -129,7 +129,7 @@ class TestBoHNormalMoreMenu(Driver):
     @pytest.mark.regression
     @pytest.mark.functional
     @pytest.mark.parametrize('test_email, test_pwd', Account.BoHCredentials)
-    def test_boh_normal_more_menu_earn_a_badge_BOH19_TC2597(self, test_email, test_pwd):
+    def test_boh_more_menu_earn_a_badge_BOH19_TC2597(self, test_email, test_pwd):
         # TEST CASES: BOH19-TC-2597
         if LoginPage.isNormalUserLoggedIn(self) is False:
             LoginPage.oktaUserLoginSkip(self, test_email, test_pwd)
